9739ab0fd1cf873d463dce4a745d22b1f4bbba1e
Ilknur Mustafov <ilknurilko@gmail.com>
2020-04-03 19:01:32 +0200

    To Do List Add with React

---
+++ src/components/App.jsx
@@ -0,0 +1,45 @@
+import React, {useState} from "react";
+
+function App() {
+
+  const [item, setItem] = useState(" ");
+  const [items, setItems] = useState([]);
+
+  function handleItem(event) {  // Save the value of the input field to the State
+    const newValue = event.target.value;
+
+    setItem(newValue);
+  }
+
+  function addItem() { // Add new item to an Array and clear the Input field
+    
+
+    setItems((prevItems) => {
+        return [...prevItems, item] //Add the new item to
+    });
+
+    setItem(""); // Clears the input field
+  }
+
+  return (
+    <div className="container">
+      <div className="heading">
+        <h1>To-Do List</h1>
+      </div>
+      <div className="form">
+
+        <input onChange={handleItem} type="text" name="item" value={item} />
+        <button onClick={addItem}><span>Add</span></button>
+
+
+      </div>
+      <div>
+        <ul>
+       { items.map((toDoItem) => <li>{toDoItem} </li>)}
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default App;
